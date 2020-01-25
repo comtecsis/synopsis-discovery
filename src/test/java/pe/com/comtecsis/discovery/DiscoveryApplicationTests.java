02977b44b9463dd1264125066557fbb5f1e88679
Elvis Perez <elperez@synopsis.ws>
2020-01-25 18:27:32 -0500

    Commit inicial

---
+++ src/test/java/pe/com/comtecsis/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.comtecsis.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
